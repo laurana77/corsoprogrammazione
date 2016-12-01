7d73f783f7082bfba63d5c8af6bda673b769378d
laurana77 <alice.fazio@unimi.it>
2016-12-01 15:23:12 +0100

    1 dicembre2

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,18 @@
-var app = angular.module('app1', []);
-	app.controller('app1Controller',['$scope', 
-		function($scope) {
+var app = angular.module('app1', ['lbServices']);
+	app.controller('app1Controller',
+		['$scope','Todo', function($scope, Todo) {
+			$scope.newTodo={
+				name:"",
+				desc:"",
+				important:false
+			}
+			$scope.aggiungitodo=function(){
+				console.log("aggiungo"+$scope.newTodo);
+			}
+			$scope.cancellatodo=function(idtodo){
+				console.log("cancella"+idtodo);
+			}
+			$scope.eseguitodo=function(idtodo){
+				console.log("eseguitodo"+idtodo);
+			}
 		}]);
